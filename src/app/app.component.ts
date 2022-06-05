0223a88aa5bc37dff719505fec7577cd3d2a07c0
aashish <aashish.singh@trangile.com>
2022-06-05 16:31:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'databinding';
+  smart= 'Angular Framework';
+
+  courses=["react jS" ,'angular',"Node js","MongoBd"];
+  show = this.courses[0];
+
+  
+    empdetails=[
+      {"name":"aashish" , "age":24 ,"city" : "ballabgarh"},
+      {"name":"vikas" , "age":25 ,"city" : "noida"},
+      {"name":"harshita" , "age":20 ,"city" : "noida"}
+    ]
+
+    abc()
+     {
+          alert("my favourite Angular frame work");
+      }
 }
+
+
+  
